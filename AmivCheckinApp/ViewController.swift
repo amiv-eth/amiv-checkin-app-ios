cff2ffdb0c3746efb2982a2835467cfcb73aebcd
Domenic Wüthrich <domenicw@student.ethz.ch>
2018-03-17 00:02:05 +0100

    Initial Commit

--- AmivCheckinApp/ViewController.swift
+++ AmivCheckinApp/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pinTitleLabel: UILabel!
+    @IBOutlet weak var pinSubmitButton: UIButton!
+    @IBOutlet weak var pinTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        self.pinTitleLabel.text = "Enter pin"
     }
 
 
+    @IBAction func pinSubmitButtonTapped(_ sender: Any) {
+        print(self.pinTextField.text!)
+    }
+    
 }
 
